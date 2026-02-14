701cae36ff8da01093fd932fc44d24ffee7d6b64
Suyash Mahesh Mejari <119077015+Suyash021@users.noreply.github.com>
2026-02-15 01:08:14 +0530

    Update deployement.py

--- deployement.py
+++ deployement.py
@@ -4,7 +4,7 @@ from pathlib import Path
 st.set_page_config(page_title="For Rekha ❤️", layout="wide")
 
 # Read the HTML file
-html_content = Path("valentine_demo.html").read_text()
+html_content = Path("valentine.html").read_text()
 
 # Display it
 st.components.v1.html(html_content, height=2000, scrolling=True)
