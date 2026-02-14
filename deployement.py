fe02b17a0ab52b6b0923d28011c1632e28b79b6b
Suyash Mahesh Mejari <119077015+Suyash021@users.noreply.github.com>
2026-02-15 00:41:46 +0530

    Update deployement.py

--- deployement.py
+++ deployement.py
@@ -315,8 +315,8 @@ html_content = """
                     </div>
                     <input type="file" id="file2" class="file-input" accept="image/*" onchange="uploadPhoto(event, 'img2', this)">
                     <img id="img2" style="display: none;">
-                    <div class="photo-label">Engagement Day</div>
-                    <div class="photo-sub">When I said "Yes" to forever 💍</div>
+                    <div class="photo-label">My Favourite Picture</div>
+                    <div class="photo-sub">When I fell harder for you</div>
                 </div>
                 
                 <!-- Photo 3 -->
